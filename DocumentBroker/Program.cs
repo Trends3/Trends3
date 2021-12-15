3d722a3a9a5bdbc2e0a01febe89263ae7feab5ed
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-12-15 12:15:05 +0100

    Add  document validator + test user

--- DocumentBroker/Program.cs
+++ DocumentBroker/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DocumentBroker.Utils;
+
+
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        Validator.validation();
+    }
+
+}
---
+++ DocumentBroker/Utils/Validator.cs
@@ -0,0 +1,45 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+
+namespace DocumentBroker.Utils
+{
+    static class Validator
+    {
+        private static bool isValid = true;
+        public static void validation()
+        {
+            //XMLCollection
+            XmlSchemaCollection collection = new XmlSchemaCollection();
+            collection.Add("", "User_control.xsd");
+
+            XmlTextReader r = new XmlTextReader("user2.xml");
+            XmlValidatingReader v = new XmlValidatingReader(r);
+            v.ValidationType = ValidationType.Schema;
+
+            //add the XMLCollection.            
+            v.Schemas.Add(collection);
+
+            v.ValidationEventHandler += new ValidationEventHandler(MyValidationEventHandler);
+
+            while (v.Read())
+            {
+                //process content
+            }
+            v.Close();
+
+            // Check if document is valid or invalid.
+            if (isValid)
+                Console.WriteLine("Document is valid");
+            else
+                Console.WriteLine("Document is invalid");
+        }
+
+        public static void MyValidationEventHandler(object sender,
+                                           ValidationEventArgs args)
+        {
+            isValid = false;
+            Console.WriteLine("Validation event\n" + args.Message);
+        }
+    }
+}
+
