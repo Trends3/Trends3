c32a73ab917a7c82c7fa20fc9b27c106e9093c6e
JaimyVA <jaimy2805ftw@gmail.com>
2021-12-19 12:05:43 +0100

    Created Queue + connection
    Created the consumer queue and connected to docker container

--- DocumentBroker/Program.cs
+++ DocumentBroker/Program.cs
@@ -1,12 +1,24 @@
-﻿using DocumentBroker.Utils;
-
-
+﻿using DocumentBroker;
+using DocumentBroker.Utils;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 class Program
 {
+    public static string URI = "amqp://guest:guest@172.17.0.3:5672";
 
     static void Main(string[] args)
     {
+       
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri(URI)
+        };
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        QueueConsumer.Consume(channel);
 
         Validator.validation();
     }
---
+++ DocumentBroker/QueueConsumer.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentBroker
+{
+    public static class QueueConsumer
+    {
+        public static void Consume(IModel channel)
+        {
+            channel.QueueDeclare("queue1",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine(message);
+            };
+            channel.BasicConsume("queue1", true, consumer);
+            Console.WriteLine("Consumer started");
+            Console.ReadLine();
+        }
+    }
+}
