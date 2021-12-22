a5e36764acc9de20ffd15a487607ef3bda6f66db
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2021-12-22 11:50:04 +0100

    Add docker compose support

--- DocumentBroker/Program.cs
+++ DocumentBroker/Program.cs
@@ -7,11 +7,15 @@ using System.Text;
 
 class Program
 {
-    //Connection naar Rabbitmq Docker Container
-    public static string URI = "amqp://guest:guest@172.17.0.3:5672";
+    //using var connection = new SqlConnection("Data Source=database; User ID=trends3;Password=trends3");
+    //  connection.Open();
+
+//Connection naar Rabbitmq Docker Container
+public static string URI = "amqp://guest:guest@172.17.0.3:5672";
 
     static void Main(string[] args)
     {
+
        
         var factory = new ConnectionFactory
         {
