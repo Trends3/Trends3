a58f6124c0b8389a5f54fa4694db01aad4d9b562
JaimyVA <jaimy2805ftw@gmail.com>
2021-12-24 09:55:24 +0100

    Added QueueProducer to solution
    Added the project QueueProducer to the DocumentBroker solution

---
+++ QueueProducer/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace QueueProducer
+{
+    static class Program
+    {
+        static void Main(String[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            QueueProducer.Publish(channel);
+        }
+    }
+}
---
+++ QueueProducer/QueueProducer.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace QueueProducer
+{
+    public static class QueueProducer
+    {
+        public static void Publish(IModel channel)
+        {
+            XmlTextReader r = new XmlTextReader("user2.xml");
+
+            string jsonData = @"{
+            'id':'1',
+            'firstName':'Ben',
+            'lastname':'Benson',
+            'gender':'Male',
+            'nationality':'Belgian',
+            'street':'Teststraat',
+            'housenumber':'1',
+            'zip':'1000',
+            'email':'ben@benson.com'
+            }";
+
+
+            channel.QueueDeclare("queue1",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            var count = 0;
+
+            while (true)
+            {
+                var message = new { Name = "Producer", Message = $"{jsonData}" };
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                channel.BasicPublish("", "queue1", null, body);
+                count++;
+                Thread.Sleep(3000);
+            }
+        }
+    }
+}
