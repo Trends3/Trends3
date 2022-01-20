3cfeaacc9c628d5d6a5531d7318466483e285bdd
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2022-01-20 13:54:08 +0100

    request class voor juiste service
    Co-Authored-By: melmelang <74968643+melmelang@users.noreply.github.com>
    Co-Authored-By: JaimyVA <74968535+JaimyVA@users.noreply.github.com>

--- DocumentBroker/Program.cs
+++ DocumentBroker/Program.cs
@@ -1,28 +1,63 @@
 ﻿using DocumentBroker;
 using DocumentBroker.Queues;
+using DocumentBroker.Request_objects;
 using RabbitMQ.Client;
 using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 class Program
 {
 
-//  using var connection = new SqlConnection("Data Source=database; User ID=SA;Password=MyVerySecurePassword$123");
-//connection.Open();
-
 //Connection naar Rabbitmq Docker Container
 
-public static string URI = @"amqp://guest:guest@172.18.0.3:5672";
+public static string URI = @"amqp://guest:guest@172.18.0.2:5672";
 
     static void Main(string[] args)
     {
+        SqlConnection connection = new SqlConnection("Data Source=Documentbroker; User ID=SA;Password=YourStrong@Passw0rd");
+        SqlCommand nonqueryCommand = connection.CreateCommand();
+        try
+        {
+            connection.Open();
+
+            nonqueryCommand.ExecuteNonQuery();
+            Console.WriteLine("Database created, now switching");
+            connection.ChangeDatabase("Documentbroker");
+
+            nonqueryCommand.CommandText = "CREATE TABLE Documentbrokers (" +
+                "Id INT PRIMARY KEY NOT NULL," +
+                "TicketId VARCHAR(100)," +
+                "ApplicationId VARCHAR(100)," +
+                "RequestType CHAR(1)" +
+                ")";
+            Console.WriteLine(nonqueryCommand.CommandText);
+            Console.WriteLine("Number of Rows Affected is: {0}", nonqueryCommand.ExecuteNonQuery());
+
+
+        }
+        catch (SqlException ex)
+        {
+
+            Console.WriteLine(ex.ToString());
+
+        }
+        finally
+        {
+
+            connection.Close();
+            Console.WriteLine("Connection Closed.");
+
+        }
 
         var factory = new ConnectionFactory
         {
             Uri = new Uri(URI)
         };
         //Consumer voor queue
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+        
+        using var connection2 = factory.CreateConnection();
+        using var channel = connection2.CreateModel();
         //QueueConsumer.Consume(channel);
 
         QueueConsumer queueConsumer = new QueueConsumer();
@@ -37,9 +72,29 @@ public static string URI = @"amqp://guest:guest@172.18.0.3:5672";
         queueConsumer.Consume(channel);
 
 
-
-
-
-
+        
+        
     }
+
+    // public void CreatePO(string filename)
+    //{
+    //    XmlSerializer serializer =
+    //    new XmlSerializer(typeof(GenerationRequest));
+    //    TextWriter writer = new StreamWriter(filename);
+    //    GenerationRequest po = new GenerationRequest();
+
+    //    serializer.Serialize(writer, po);
+    //    writer.Close();
+    //}
+
+    //public void ReadPO(string filename)
+    //{
+    //    XmlSerializer serializer = new XmlSerializer(typeof(GenerationRequest));
+   
+    //    FileStream fs = new FileStream(filename, FileMode.Open);
+    //    GenerationRequest po;
+    //    po = (GenerationRequest)serializer.Deserialize(fs);
+
+    //    Console.WriteLine(po);
+    //}
 }
---
+++ DocumentBroker/Request_objects/GenerationRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DocumentBroker.Request_objects
+{
+    [XmlRoot("GenerationRequest")]
+     class GenerationRequest
+    {
+
+        // ticket 
+        // Payload = content van message
+        // 
+
+        [XmlElement("Ticket")]
+        public Guid Ticket = Guid.NewGuid();
+
+        [XmlElement("Payload")]
+        public Payload ? payload { get; set; }
+
+    }
+
+    internal class Payload
+    {
+        // payload van queue 
+        [XmlElement("Message")]
+        public string message = "test";
+    }
+
+
+}
